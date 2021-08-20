9f598ed920b3359713d1127452fe2ddf1a51d39d
Dione dos Santos <73508786+dione-is@users.noreply.github.com>
2021-08-20 11:38:16 -0300

    exercicio apresentado em aula
    inserir elemento em uma estrutura de dados do tipo arvore binaria, apresentar em pre-ordem, em -ordem e pos ordem, realizar busca de elemento por valor, acrescentar porcentagem aos valores de todos elementos.

--- exercicio.cpp
+++ exercicio.cpp
@@ -50,6 +50,20 @@ void preOrdem(TNo *raiz)
 	}
 }
 
+void preOrdem_NaoRecursiva(TNo *raiz){
+	
+	TNo *aux;
+	aux = raiz;
+	while(aux != NULL){
+		
+		printf("\nNome: %s",aux->elemento.nome);
+		printf("\nValor: %d", aux->elemento.valor);
+		printf("\n=========================");
+		if(aux->esq->elemento.valor < aux->elemento.valor)
+			aux = aux->esq;
+	}
+}
+
 void emOrdem(TNo *raiz)
 {
 	if (raiz != NULL)
@@ -107,23 +121,39 @@ int buscar (TNo *raiz, int valor)
 	}
 }
 
-	void maiorXmenor(TNo* raiz,int *maior, int *menor){
+	int menor(TNo* raiz){
 		
-		if (raiz != NULL)
-		{
-		emOrdem(raiz->esq);
-		printf("\nNome: %s",raiz->elemento.nome);
-		printf("\nValor: %d", raiz->elemento.valor);
-		printf("\n=========================");
-		if(menor == 0 || menor > raiz->esq.elemento.valor){
-			menor = raiz->esq.elemento.valor;
-		}				
-		emOrdem(raiz->dir);
-		if(maior < raiz->dir.elemento.valor){
-			maior = raiz->dir.elemento.valor;
+		int menorvalor = 0;
+		if (raiz != NULL){
+			menor(raiz->esq);
+			if(raiz->esq == NULL) {
+				printf("Produto de menor valor: %s\n",raiz->elemento.nome);
+				printf("valor: %d\n", raiz->elemento.valor);
+				menorvalor = raiz->elemento.valor;
+				return menorvalor;
+			}
+		
 		}
 	}
 	
+	void maior(TNo* raiz){
+		if(raiz != NULL){
+			maior(raiz->dir);
+				if(raiz->dir == NULL){
+					printf("\nProduto de maior Valor: %s",raiz->elemento.nome);
+					printf("\nValor: %d", raiz->elemento.valor);
+				}
+		}
+	}
+	
+	void ajustarPorcentagem(TNo* raiz, int valor) {
+		
+		if (raiz != NULL)
+		{
+			raiz->elemento.valor = (raiz->elemento.valor +(valor/100 * raiz->elemento.valor));
+			ajustarPorcentagem(raiz->esq,valor);					
+			ajustarPorcentagem(raiz->dir,valor);
+		}
 	}
 
 int main()
@@ -142,6 +172,10 @@ int main()
 		printf("\n 3 - Apresentar (em-ordem)");
 		printf("\n 4 - Apresentar (pos-ordem)");
 		printf("\n 5 - Consultar");
+		printf("\n 6 - Produto Menor Valor");
+		printf("\n 7 - Produto Maior Valor");
+		printf("\n 8 - Acrescentar ajustes de valor");
+		printf("\n 9 - Apresentar (Pre-ordem) Nao recursiva");
 		printf("\n 0 - Sair");
 		printf("\n Escolha uma opcao: ");
 		scanf("%d", &opcao);
@@ -187,6 +221,29 @@ int main()
 					}
 					break;
 				}
+			case 6:
+				{
+					int valorMenor =0;
+					valorMenor= menor(raiz);
+					//printf("O menor  valor e: %d",valorMenor);
+					break;
+				}
+			case 7:
+				{
+					maior(raiz); break;
+				}
+			case 8:
+				{
+					int porcentagem = 0;
+					printf("\nQual a porcetagem deseja acrescentar nos valores dos produtos ?");
+					scanf("%d",&porcentagem);
+					
+					ajustarPorcentagem(raiz,porcentagem);break;
+				}
+			case 9:
+				{
+					preOrdem_NaoRecursiva(raiz);break;
+				}
 			
 		}
 	}while (opcao != 0);
@@ -195,7 +252,9 @@ int main()
 
 
 
-ponteiro menor *menor, *menor     paramentro teste (raiz,&maior,&menor)
+/*ponteiro menor *menor, *menor     paramentro teste (raiz,&maior,&menor)
 
 								void teste(TNo raiz, &*maior, &*menor);
 								*aux->elemento.valor
+								
+								*/ 
